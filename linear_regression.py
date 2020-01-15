9604216c571cca464101b18d71fcce3f8e63b40e
Benoit GEORGES <bgeorges@e1r13p10.42.fr>
2020-01-15 21:12:35 +0100

    algo finished

--- linear_regression.py
+++ linear_regression.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+
 
 def openCSV():
     data = pd.read_csv("data.csv")
@@ -7,24 +9,58 @@ def openCSV():
     
     return prices, kms
 
-def costFunction(theta0, price):
-    m = len(prices)
-    prediction = 0
-    for price in prices: 
-        prediction += theta0 + theta1 * X - price
-    print(prediction)
+def displayFunction(kms, theta0, theta1):
+    a = []
+    for i in range(len(kms)):
+        a.append(kms[i] * theta1 + theta0)
+    return a
 
-def linearRegression(prices, kms):
+def costFunction(theta0, theta1, prices, kms):
+    m = len(prices)
+    cost = 0
+    for i in range(m):
+        cost += (1/m) * ((theta0 + theta1 * kms[i]) - (prices[i]))
+    return(cost)
+
+def normalizeFonction(datas):
+    normalizedDatas = []
+    for i in range(len(datas)):
+        normalizedDatas.append((datas[i]) / (max(datas)))
+    return normalizedDatas
+
+def costFunction_2(theta0, theta1, prices, kms):
+    m = len(prices)
+    cost = 0
+    for i in range(m):
+        cost += (1/m) * (((theta0 + theta1 * kms[i]) - (prices[i])) * kms[i])
+    return(cost)
+
+def gradientDescent(prices, kms):
     theta0 = 0
     theta1 = 0
-    X = 0
-    LearningRate = 0.0000000000015
-    costFunction(theta0, prices)
-    
+    pricesNormalized = normalizeFonction(prices)
+    kmsNormalized = normalizeFonction(kms)
+    learningRate = 1.5
+    m = len(prices)
+    while(True):
+        temp0 = theta0
+        temp1 = theta1        
+        theta0 = temp0 - learningRate * ((1 / m) * costFunction(temp0, temp1, pricesNormalized, kmsNormalized))
+        theta1 = temp1 - learningRate * ((1 / m) * costFunction_2(temp0, temp1, pricesNormalized, kmsNormalized))
+        if(temp0 == theta0 and theta1 == temp1):
+            break
+    return theta0, theta1
+        
 
 if __name__ == '__main__':
     prices, kms = openCSV()
-    linearRegression(prices, kms)
-    
-    
-    
+    theta0, theta1 = gradientDescent(prices, kms)
+    theta0 = theta0 * max(prices)
+    theta1 = theta1 * (max(prices)) / max(kms)
+    axes = plt.axes()
+    axes.grid() # dessiner une grille pour une meilleur lisibilité du graphe
+    plt.scatter(kms, prices) # X et Y sont les variables qu'on a extraite dans le paragraphe précédent
+    plt.xlabel("Kilometrage")
+    plt.ylabel("Prix")
+    plt.plot(kms, displayFunction(kms, theta0, theta1))
+    plt.show()
---
+++ resolve.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 15 20:47:05 2020
+
+@author: bgeorges
+"""
+
+
+if __name__ == '__main__':
+    
+    
+
